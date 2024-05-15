f23efeab355c26798034da80c3aa2fe7abfb220e
zzmeow0516 <zzmeow0516@163.com>
2024-05-15 18:30:57 +0800

    start broadcastReceiver

--- app/src/main/java/com/example/learn_android_9/MainActivity.kt
+++ app/src/main/java/com/example/learn_android_9/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.learn_android_9
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "mylog_BroadcastReceiver"
+
+    //需要写成类的成员变量，方便我们在onCreate和onDestroy中调用
+    lateinit var timeChangeBroadcastReceiver: TimeChangeBroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +27,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.intent.action.TIME_TICK")
+        timeChangeBroadcastReceiver = TimeChangeBroadcastReceiver()
+        registerReceiver(timeChangeBroadcastReceiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(timeChangeBroadcastReceiver)
+    }
+
+    inner class TimeChangeBroadcastReceiver() : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Toast.makeText(context, "time changed", Toast.LENGTH_SHORT).show()
+            Log.v(TAG, "time changed")
         }
     }
 }
